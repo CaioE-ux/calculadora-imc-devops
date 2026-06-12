ef8b88680b0b3566ca7a84c315814fd2133fcf00
CaioE-ux <caio.enrique@sempreceub.com>
2026-06-12 00:39:09 +0000

    criacao

--- imc.test.js
+++ imc.test.js
@@ -1,6 +1,15 @@
 const calcularIMC = require('./imc');
 
-test('Calcula o IMC corretamente para peso 80 e altura 2', () => {
-    // 80 / (2 * 2) = 20.00
-    expect(calcularIMC(80, 2)).toBe("20.00");
+describe('Função calcularIMC', () => {
+    test.each([
+        [80, 2, '20.00'],
+        [70, 1.75, '22.86'],
+        [90, 1.80, '27.78'],
+        [60, 1.60, '23.44']
+    ])(
+        'deve retornar %s/%s² = %s',
+        (peso, altura, resultadoEsperado) => {
+            expect(calcularIMC(peso, altura)).toBe(resultadoEsperado);
+        }
+    );
 });
